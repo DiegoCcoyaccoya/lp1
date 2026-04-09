c972e81726e19b787069b7771b97eb4518bec354
DiegoCcoyaccoya <dccoyaccoyah@ulasalle.edu.pe>
2026-04-08 20:58:29 -0500

    Update omegaup.cpp
    Segundo avance del problema Contratación Dinámica

--- lab01/omegaup.cpp
+++ lab01/omegaup.cpp
@@ -2,5 +2,13 @@
 #include <vector>
 using namespace std;
 
+int main() {
+    int N, K;
+    cin >> N >> K;
 
+    vector<int> a(N);
+    for(int i = 0; i < N; i++) {
+        cin >> a[i];
+    }
 
+   
