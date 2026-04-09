c5cb95eafbf68af85669918e722fad98ab7285ff
DiegoCcoyaccoya <dccoyaccoyah@ulasalle.edu.pe>
2026-04-08 20:59:06 -0500

    Update omegaup.cpp
    Problema de Omega Up Contratación dinámica completada

--- lab01/omegaup.cpp
+++ lab01/omegaup.cpp
@@ -10,5 +10,22 @@ int main() {
     for(int i = 0; i < N; i++) {
         cin >> a[i];
     }
+ for(int i = 0; i <= N - K; i++) {
+        int maximo = a[i];
+
+        for(int j = i; j < i + K; j++) {
+            if(a[j] > maximo) {
+                maximo = a[j];
+            }
+        }
+
+        cout << maximo;
+        if(i < N - K) cout << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
 
    
