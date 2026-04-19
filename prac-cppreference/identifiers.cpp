ddb5a70526064422096a6e72b29a0f4058ea639c
DiegoCcoyaccoya <dccoyaccoyah@ulasalle.edu.pe>
2026-04-19 11:33:54 -0500

    Create identifiers.cpp

---
+++ prac-cppreference/identifiers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int edadUsuario = 19;
+    int anioNacimiento = 2006;
+
+    std::cout << "Edad: " << edadUsuario << std::endl;
+    std::cout << "Año de nacimiento: " << anioNacimiento << std::endl;
+
+    return 0;
+}
